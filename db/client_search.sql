71069712bcfb510412832eccc97ebe7e47a0644d
Gekko Wrld <gekkowrld@gmail.com>
2025-04-25 19:42:04 +0300

    fix: remove redundant sql code
    Signed-off-by: Gekko Wrld <gekkowrld@gmail.com>

--- db/client_search.sql
+++ db/client_search.sql
@@ -9,10 +9,4 @@ FROM
 WHERE
   client_fts MATCH ?
 ORDER BY
-  rank;-- Add search functionality
-
-SELECT id, first_name, last_name
-FROM client_fts
-WHERE client_fts MATCH ?
-ORDER BY rank;
-
+  rank;
